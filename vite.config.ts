0cca436f44afbd89ed1224ac5b8aeb6ad3f443eb
zoodogood <zoodogood@gmail.com>
2025-04-05 11:07:56 +0300

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Samii_luchii_project",
   plugins: [
     vue(),
     vueJsx(),
