c394fb4a5cfef1b6e2f7ac45c9e63c08f2e43e49
zoodogood <zoodogood@gmail.com>
2025-04-05 17:47:38 +0300

    Update demo

--- src/novel/_scene/main.ts
+++ src/novel/_scene/main.ts
@@ -1,22 +1,31 @@
 import { _makeUtilsBy } from "@/novel/_scene/makeUtils";
+import { useSceneState } from "@/novel/scene/singleton";
 import { sleep } from "@/sleep";
 import { SECOND } from "@/time/constants";
 import type { Ref } from "vue";
 
 export async function _run_scene(texthere_text: Ref<string>, deadline_end: Function) {
-	const {write, write_pause, empty, currentText, glitch} = _makeUtilsBy(texthere_text, deadline_end)
+	const {write: w, use_pause, empty, currentText, glitch, switch_bold} = _makeUtilsBy(texthere_text, deadline_end)
+	const scene = useSceneState();
 	while (true) {
-		await write("Lorem my loved lorem.");
+		scene.updateBackgroundRule("#ffffbb");
+		await w("Lorem my loved lorem.");
 		await sleep(SECOND);
 		await glitch(currentText(), "");
-		await write("<Hop-hop-hop>");
+		await w("<Hop-hop-hop>");
 		await sleep(50);
 		empty();
-		await write(
-			`P(1)3P(1).P(1) 2P(1).P(1). P(2) 1P(1).P(1).. P(3)`.replace(
+		scene.updateBackgroundRule("#ffbbff");
+		await w(
+			`P(1)3P(1).P(1) 2P(1).P(1). P(2) 1P(1).P(1).P(1). P(3)`.replace(
 				/P\((?<time>\d)\)/g,
-				(_0, _1, _2, _3, { time }) => write_pause(SECOND * time)
+				(_0, _1, _2, _3, { time }) => use_pause(SECOND * time)
 			)
 		);
+		await w(switch_bold());
+		await w("Lorem my loved lorem.");
+		await w(switch_bold());
+		await w(".");
+		empty();
 	}
 }
