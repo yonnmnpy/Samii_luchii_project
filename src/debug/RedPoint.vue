af610844849a500b672a40dfe4d31803a402e24d
zoodogood <zoodogood@gmail.com>
2025-04-11 09:59:35 +0300

    Add RedPoint for debug

---
+++ src/debug/RedPoint.vue
@@ -0,0 +1,25 @@
+<script setup lang = "ts">
+	const {left, top} = defineProps<{
+		left: string
+		top: string
+	}>()
+</script>
+
+
+<template>
+	<div class="point"></div>
+</template>
+
+
+<style scoped>
+	.point {
+		position: fixed;
+		display: block;
+		width: 7px;
+		height: 7px;
+		background: red;
+		border-radius: 50%;
+		left: v-bind("left");
+		top: v-bind("top");
+	}
+</style>
