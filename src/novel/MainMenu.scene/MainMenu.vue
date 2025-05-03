ba275689b0f32f8d1b20efad634c7129b2c45ac8
zoodogood <zoodogood@gmail.com>
2025-05-03 21:01:53 +0300

    Navigate to «meet the authors» page

--- src/novel/MainMenu.scene/MainMenu.vue
+++ src/novel/MainMenu.scene/MainMenu.vue
@@ -4,13 +4,15 @@ import JustPointerBall from '@/JustPointerBall/JustPointerBall.vue';
 import { useSceneState } from '@/novel/Game/current_scene_state';
 import { sleep } from '@/sleep';
 import { SECOND } from '@/time/constants';
+import { Routes } from '../router';
+
 
 const scene = useSceneState();
 
 async function runGame() {
 	scene.updateBackgroundRule("#000")
 	await sleep(SECOND * 3)
-	scene.mainMenuOpened = false
+	scene.currentRoute = Routes.Scene
 }
 
 const { x, y } = usePointer()
@@ -21,13 +23,16 @@ const { x, y } = usePointer()
 	<x-main-menu>
 			<button @click="runGame">Запустить</button> 
 			<span>|</span>
-			<button>От авторов</button>
+			<button @click="scene.currentRoute = Routes.AuthorsText">От авторов</button>
 	</x-main-menu>
 	<JustPointerBall :x="x!" :y="y!"/>
 </template>
 
 
 <style scoped>
+span {
+	user-select: none;
+}
 x-main-menu {
 	position: fixed;
 	left: 50%;
